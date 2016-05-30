19b2f7f5eecf3aedbdea41f54c8d06720f6ce36f
tangyi1234 <154542607@qq.com>
2016-05-30 17:19:16 +0800

    Initial Commit

---
+++ CellTypeTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CellTypeTableView
+//
+//  Created by 汤义 on 16/5/30.
+//  Copyright © 2016年 汤义. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
